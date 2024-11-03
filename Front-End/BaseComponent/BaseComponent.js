b5fb8ea17754eb056dd6ae6bbfe0b410df9cab7a
Gin Park <36610088+ginpks@users.noreply.github.com>
2024-11-03 13:07:31 -0500

    Update and rename Front-End/BaseComponent.js to Front-End/BaseComponent/BaseComponent.js
    Added clarifying comments about what this file is used for.

--- Front-End/BaseComponent/BaseComponent.js
+++ Front-End/BaseComponent/BaseComponent.js
@@ -1,4 +1,6 @@
-// base component file taken from TASKS-V3 from lecture
+// base component file taken from TASKS-V3 from lecture as an example to use.
+// BaseComponent enforces a structure, ensuring that all components derived from BaseComponent will provide their own rendering logic.
+
 
 export class BaseComponent {
   constructor() {
@@ -15,6 +17,7 @@ export class BaseComponent {
     throw new Error('render method not implemented');
   }
 
+  // a method for loading CSS stylesheets (loadCSS). 
   loadCSS(fileName) {
     if (this.cssLoaded) return;
 
@@ -25,12 +28,13 @@ export class BaseComponent {
     document.head.appendChild(link);
     this.cssLoaded = true;
   }
-
+  
+  // facilitates the creation and dispatching of custom events, allowing components to communicate with other parts of the application.
   dispatchCustomEvent(eventName, detail = {}) {
     const event = new CustomEvent(eventName, { detail });
     this.parent.dispatchEvent(event);
   }
-
+ // allows the component to listen for events from its parent, enabling interaction and responsive behavior within the component hierarchy.
   listenToEvent(eventName, callback) {
     this.parent.addEventListener(eventName, callback);
   }
