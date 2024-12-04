f67558a7a16db3cde3fe88ea8ec143dc4bfff6f7
ginpks <ginhpark@gmail.com>
2024-12-04 00:02:44 -0500

    implemented user model

--- Back-End/authentication/user.js
+++ Back-End/authentication/user.js
@@ -1,1 +1,26 @@
+// import { Sequelize, DataTypes } from "sequelize";
+import { Sequelize, DataTypes } from '@sequelize/core';
+import { SqliteDialect } from '@sequelize/sqlite3';
 
+// Create a new instance of Sequelize using sqlite as our database
+// and storing it in authentication.sqlite
+const sequelize = new Sequelize({
+  dialect: SqliteDialect,
+  storage: 'authentication.sqlite',
+});
+
+
+// Defining the User model
+const User = sequelize.define("User", {
+  username: { type: DataTypes.STRING, unique: true, allowNull: false },
+  password: { type: DataTypes.STRING },
+  googleId: { type: DataTypes.STRING },
+  role: { type: DataTypes.STRING, defaultValue: "user" }, // Roles: 'user', 'admin'
+});
+
+
+// Create the table if it doesn't exist or update existing table to match model definition above
+await sequelize.sync();
+
+// Export the User model to make database accessible for other components of our app.
+export default User;
