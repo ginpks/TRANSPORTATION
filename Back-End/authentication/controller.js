7693aebcc4b6573fd4cddc73de24402d3f262977
ma9c0 <125161461+ma9c0@users.noreply.github.com>
2024-12-05 04:38:34 -0500

    logout
    when the user click on the logout botton, it should log the user out and redirect the user back to login page

--- Back-End/authentication/controller.js
+++ Back-End/authentication/controller.js
@@ -1,3 +1,16 @@
 import bcrypt from "bcryptjs";
 import dotenv from "dotenv";
 import User from "../Back-End/authentication/user.js";
+
+
+const factoryResponse = (status, message) => ({ status, message });
+
+export const logout = (req, res) => {
+    req.logout(function (err) {
+        if (err) {
+            res.json(factoryResponse(500, "Logout failed"));
+            return;
+        }
+        res.json(factoryResponse(200, "Logout successful"));
+    });
+};
