a73b06e409006b2f6a57d1a750741df68a5fbe85
sheyanyu <sheyanyu@umass.edu>
2024-12-07 13:34:01 -0500

    Implement getORcreate session function #79: function is set up for back end logic

--- Back-End/chat/routes.js
+++ Back-End/chat/routes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import {
     saveMessage,
-    getChatHistory
+    getChatHistory,
+    getSession
 } from './controller.js';
 
 const router = express.Router();
 
+router.post("/session", getSession);
 router.post("/messages", saveMessage);
 router.get("/messages/:session_id", getChatHistory);
-
 
 export default router;
